97a53e1d52e25a9f87f16114e8ec01ea6fb934ef
Jinmoyee Thakuria <110217586+Jinmoyee@users.noreply.github.com>
2024-02-22 01:58:51 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    return (
+        <div className='underline text-4xl font-semibold'>App</div>
+    )
+}
+
+export default App
